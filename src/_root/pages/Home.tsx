a62499cd82f733fbed6faf45287d5ecc2df85ffb
brunofbpaula <deepcooper@outlook.com>
2025-12-29 20:17:43 -0300

    rm: old components

--- src/_root/pages/Home.tsx
+++ src/_root/pages/Home.tsx
@@ -1,19 +1,6 @@
-import PlayerCard from '@/components/shared/PlayerCard';
-import SkillChart from '@/components/shared/SkillChart';
-import SkillsModal from '@/components/shared/SkillsModal';
-
 const Home = () => {
   return (
-    <>
-      <div className='utility-box'>
-        <div></div>
-        <SkillsModal/>
-      </div>
-      <div className='player-box'>
-        <PlayerCard/>
-        <SkillChart/>
-      </div>
-    </>
+    <div></div>
   )
 }
 
